d953cb84c28764c89b9cc1b7caface017b5b1caf
zhtlancer <zhtlancer@285eb7d0-55e3-11de-8ad8-e3edb75ee213>
2009-10-29 06:28:01 +0000

    ECB

--- infosec/hw2/src1/AES.h
+++ infosec/hw2/src1/AES.h
@@ -2,9 +2,10 @@
 #define __AES_H__
 
 #define ROW_COUNT 4
+#include "types.h"
 
-void AESEncrypt(ubyte_t * output, ubyte_t * key, int Nb, int Nk, int Nr);
+void AESEncrypt(ubyte_t * output, const ubyte_t * key, int Nb, int Nk, int Nr);
 
-void AESDecrypt(ubyte_t * output, ubyte_t * key, int Nb, int Nk, int Nr);
+void AESDecrypt(ubyte_t * output, const ubyte_t * key, int Nb, int Nk, int Nr);
 
 #endif
--- infosec/hw2/src1/ECB.c
+++ infosec/hw2/src1/ECB.c
@@ -1,11 +1,46 @@
-#define Nk 4
-#define Nb 4
-#define Nr 10
+#define NK 4
+#define NB 4
+#define NR 10
 
-void ECBEncrypt(const ubyte_t *plain, const ubyte_t *key, ubyte_t **cipher)
+#include <string.h>
+#include <malloc.h>
+
+#include "PlainText.h"
+#include "AES.h"
+
+int ECBEncrypt(const ubyte_t *plain, const ubyte_t *key, ubyte_t **cipher, int length)
 {
+	int block_num;
+	int i;
+	ubyte_t * ptext;
+
+	block_num = FormatPlainText(plain, cipher, length);
+
+	ptext = *cipher;
+
+	for (i = 0; i < block_num; ++i) {
+		AESEncrypt(ptext+(i*BLOCK_BYTE_SIZE), key, NB, NK, NR);
+	}
+
+	return block_num * BLOCK_BYTE_SIZE;
 }
 
-void ECBDecrypt(const ubyte_t *cipher, const ubyte_t *key, ubyte_t **plain)
+int ECBDecrypt(const ubyte_t *cipher, const ubyte_t *key, ubyte_t **plain, int length)
 {
+	int plain_length;
+	ubyte_t * ptemp;
+	int i;
+	int block_num;
+
+	ptemp = (ubyte_t *) malloc(sizeof(ubyte_t)*length);
+	memcpy(ptemp, cipher, sizeof(ubyte_t)*length);
+
+	block_num = length / BLOCK_BYTE_SIZE;
+	for (i = 0; i < block_num; ++i) {
+		AESDecrypt(ptemp+(i*BLOCK_BYTE_SIZE), key, NB, NK, NR);
+	}
+	
+	plain_length = ParsePlainText(ptemp, plain, length);
+	free(ptemp);
+	return plain_length;
 }
