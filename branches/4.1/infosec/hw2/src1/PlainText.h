44f9d3b689f7c24884a38badb88ad82a6f626f65
zhtlancer <zhtlancer@285eb7d0-55e3-11de-8ad8-e3edb75ee213>
2009-10-29 05:22:33 +0000

    PlainText

--- branches/4.1/infosec/hw2/src1/PlainText.h
+++ branches/4.1/infosec/hw2/src1/PlainText.h
@@ -1,6 +1,11 @@
 #ifndef __PLAINTEXT_H__
 #define __PLAINTEXT_H__
 
+#define BLOCK_BIT_SIZE 128
+#define BLOCK_BYTE_SIZE 16
 
+int FormatPlainText(const ubyte_t *text, ubyte_t **out, int length);
+
+int ParsePlainText(const ubyte_t *text, ubyte_t **out, int length);
 
 #endif
