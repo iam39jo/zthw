eda213810ead00aa3693454934d5182e9c4b1ce8
zhtlancer <zhtlancer@285eb7d0-55e3-11de-8ad8-e3edb75ee213>
2009-11-09 07:57:29 +0000

    fix header format

--- branches/lab-prj/MLC/code/nand_ecc_bch.h
+++ branches/lab-prj/MLC/code/nand_ecc_bch.h
@@ -1,4 +1,8 @@
+#ifndef __NAND_ECC_BCH_HEADER__
+#define __NAND_ECC_BCH_HEADER__
 
 void bch_encoder(const unsigned char *indata, unsigned char *bch_code);
 
 int bch_decoder(unsigned char *indata, const unsigned char *bch_code);
+
+#endif
