05f8738e679ea23160d1868490a1b278bef1b61a
zhtlancer <zhtlancer@285eb7d0-55e3-11de-8ad8-e3edb75ee213>
2009-11-12 11:54:28 +0000

    test finished

---
+++ infosec/hw2/src1/run-test.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+MODE="ECB CBC CFB OFB CTR"
+TESTFILE="text.txt"
+
+echo $MODE
+
+for m in $MODE
+do
+	echo "Testing $m mode"
+	echo "./aes -m $m -e -i $TESTFILE -o $m-c.dat"
+	./aes -m $m -e -i $TESTFILE -o $m-c.dat
+	echo "./aes -m $m -d -i $TESTFILE -o $m-p.dat"
+	./aes -m $m -d -i $m-c.dat -o $m-p.dat
+	echo ""
+	echo "$m mode test finished"
+	echo ""
+done
+
