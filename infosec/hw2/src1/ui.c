96ad36d7eefb227bbe5f6781aa447fda12e043d0
zhtlancer <zhtlancer@285eb7d0-55e3-11de-8ad8-e3edb75ee213>
2009-11-12 10:34:30 +0000

    finish ui

--- infosec/hw2/src1/ui.c
+++ infosec/hw2/src1/ui.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <malloc.h>
 
 #include "key.h"
 #include "ECB.h"
@@ -7,12 +8,9 @@
 #include "CFB.h"
 #include "OFB.h"
 
-#define USAGE "Usage: ./crypt -m mode -e[-d] < in_data.txt > out_data.txt"
+#define USAGE "Usage: ./crypt -m mode -e[-d] -i in_data.txt -o out_data.txt\n\tAvailable mode: ECB/CBC/CFB/OFB/CTR"
 
 #define DATA_BUF 102400
-
-ubyte_t *indata_buf[DATA_BUF];
-
 
 enum CryptMode { MINVALID = -1, ECB = 0, CBC, CFB, OFB, CTR } crypt_mode;
 enum CryptDirection { DINVALID = -1, ENCRYPT = 0, DECRYPT } crypt_direction;
@@ -24,6 +22,10 @@ const char *getModeName(int mode)
 	return modeName[mode];
 }
 
+int openInFIle(const char *name, ubyte_t **buf);
+
+int writeToFile(const char *name, const ubyte_t *buf, int size);
+
 int main(int argc, char *argv[])
 {
 	ubyte_t *org_data;
@@ -31,11 +33,15 @@ int main(int argc, char *argv[])
 	ubyte_t *p_data;
 	int i;
 	int in_data_len;
-	ubyte_t c_temp;
+	int c_temp;
 	int c_len;
 	int p_len;
 	int mode;
 	int direction;
+	int in_file;
+	const char *in_filename;
+	int out_file;
+	const char *out_filename;
 	int invalid_para;
 	char *para_temp;
 	FILE *f1, *f2, *f3;
@@ -43,9 +49,11 @@ int main(int argc, char *argv[])
 	invalid_para = 0;
 	mode = MINVALID;
 	direction = DINVALID;
+	in_file = 0;
+	out_file = 0;
 	for (i = 1; i < argc; i++) {
 		if(argv[i][0] == '-') {
-			if (argv[i][1] == 'm') {
+			if (argv[i][1] == 'm' || argv[i][1] == 'M') {
 				i++;
 				if (i >= argc) {
 					fprintf(stderr, "Invalid parameter for '-m'\n");
@@ -54,7 +62,9 @@ int main(int argc, char *argv[])
 				}
 
 				para_temp = strdup(argv[i]);
-				strupr(para_temp);
+				in_data_len = strlen(para_temp);
+				while (--in_data_len >= 0)
+					para_temp[in_data_len] = tolower(para_temp[in_data_len]);
 
 				if (strcmp(para_temp, "ecb") == 0) {
 					mode = ECB;
@@ -75,10 +85,34 @@ int main(int argc, char *argv[])
 				}
 				free(para_temp);
 
-			} else if (argv[i][1] == 'd') {
-				direction = crypt_direction.DECRYPT;
-			} else if (argv[i][1] == 'e') {
-				direction = crypt_direction.Encrypt;
+			} else if (argv[i][1] == 'd' || argv[i][1] == 'D') {
+				direction = DECRYPT;
+
+			} else if (argv[i][1] == 'e' || argv[i][1] == 'E') {
+				direction = ENCRYPT;
+
+			} else if (argv[i][1] == 'i' || argv[i][1] == 'I') {
+				i++;
+				if (i >= argc) {
+					fprintf(stderr, "Invalid parameter for '-i'\n");
+					invalid_para = 1;
+					break;
+				}
+
+				in_file = 1;
+				in_filename = argv[i];
+
+			} else if (argv[i][1] == 'o' || argv[i][1] == 'O') {
+				i++;
+				if (i >= argc) {
+					fprintf(stderr, "Invalid parameter for '-o'\n");
+					invalid_para = 1;
+					break;
+				}
+
+				out_file = 1;
+				out_filename = argv[i];
+
 			} else {
 				fprintf(stderr, "Unrecognized option '%s'\n", argv[i]);
 				invalid_para = 1;
@@ -87,70 +121,111 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	if (invalid_para ||
-			mode == crypt_mode.INVALID ||
-			direction == crypt_direction.INVALID) {
+	if (invalid_para ||	mode == MINVALID || direction == DINVALID ||
+			in_file == 0 || out_file == 0) {
 		printf("%s\n", USAGE);
 		return 10;
 	}
 
-	in_data_len = 0;
-	while ((c_temp = getchar()) != EOF)
-		indata_buf[in_data_len++] = c_temp;
+	in_data_len = openInFIle(in_filename, &org_data);
+
+	if (in_data_len == -1) {
+		printf("%s\n", USAGE);
+		return 11;
+	}
 
 	// ENCRYPT PROCESS
-	if (direction == crypt_direction.ENCRYPT) {
+	if (direction == ENCRYPT) {
 		fprintf(stderr, "Encrypting data with %s mode...\n", getModeName(mode));
 		switch (mode) {
-			case crypt_mode.ECB:
-				c_len = ECBEncrypt(indata_buf, test_key, &c_data, in_data_len);
+			case ECB:
+				c_len = ECBEncrypt(org_data, test_key, &c_data, in_data_len);
 				break;
-			case crypt_mode.CBC:
-				c_len = CBCEncrypt(indata_buf, test_key, test_V, &c_data, in_data_len);
+			case CBC:
+				c_len = CBCEncrypt(org_data, test_key, test_V, &c_data, in_data_len);
 				break;
-			case crypt_mode.CFB:
-				c_len = CFBEncrypt(indata_buf, test_key, test_V, &c_data, in_data_len);
+			case CFB:
+				c_len = CFBEncrypt(org_data, test_key, test_V, &c_data, in_data_len);
 				break;
-			case crypt_mode.OFB:
-				c_len = OFBEncrypt(indata_buf, test_key, &c_data, in_data_len);
+			case OFB:
+				c_len = OFBEncrypt(org_data, test_key, test_V, &c_data, in_data_len);
 				break;
-			case crypt_mode.CTR:
-				c_len = CTREncrypt(indata_buf, test_key, &c_data, in_data_len);
+			case CTR:
+				c_len = CTREncrypt(org_data, test_key, test_V, &c_data, in_data_len);
 				break;
 		}
-		fprintf(stderr, "Encrypt finished. Cipher text length is %d\n");
+		fprintf(stderr, "Encrypt finished. Cipher text length is %d\n", c_len);
 
-		for (i = 0; i < c_len; i++)
-			fprintf(stdout, "%c", c_data[i]);
-
+		/*for (i = 0; i < c_len; i++)*/
+			/*fprintf(stdout, "%c", c_data[i]);*/
+		writeToFile(out_filename, c_data, c_len);
 		free(c_data);
-
 	} else {
 		fprintf(stderr, "Decrypting data with %s mode...\n", getModeName(mode));
 		switch (mode) {
-			case crypt_mode.ECB:
-				p_len = ECBDecrypt(indata_buf, test_key, &p_data, in_data_len);
+			case ECB:
+				p_len = ECBDecrypt(org_data, test_key, &p_data, in_data_len);
 				break;
-			case crypt_mode.CBC:
-				p_len = CBCDecrypt(indata_buf, test_key, test_V, &p_data, in_data_len);
+			case CBC:
+				p_len = CBCDecrypt(org_data, test_key, test_V, &p_data, in_data_len);
 				break;
-			case crypt_mode.CFB:
-				p_len = CFBDecrypt(indata_buf, test_key, test_V, &p_data, in_data_len);
+			case CFB:
+				p_len = CFBDecrypt(org_data, test_key, test_V, &p_data, in_data_len);
 				break;
-			case crypt_mode.OFB:
-				p_len = OFBDecrypt(indata_buf, test_key, test_V, &p_data, in_data_len);
+			case OFB:
+				p_len = OFBDecrypt(org_data, test_key, test_V, &p_data, in_data_len);
 				break;
-			case crypt_mode.CTR:
-				p_len = CTRDecrypt(indata_buf, test_key, test_V, &p_data, in_data_len);
+			case CTR:
+				p_len = CTRDecrypt(org_data, test_key, test_V, &p_data, in_data_len);
 				break;
 		}
-		fprintf(stderr, "Decrypt finished. Plain text length is %d\n");
+		fprintf(stderr, "Decrypt finished. Plain text length is %d\n", p_len);
 
-		for (i = 0; i < p_len; i++)
-			fprintf(stdout, "%c", p_data[i]);
-
+		/*for (i = 0; i < p_len; i++)*/
+			/*fprintf(stdout, "%c", p_data[i]);*/
+		writeToFile(out_filename, p_data, p_len);
 		free(p_data);
 	}
 
+	free(org_data);
 	return 0;
+}
+
+int openInFIle(const char *name, ubyte_t **buf)
+{
+	FILE *fp;
+	int fsize;
+	ubyte_t *ptemp;
+
+	fp = fopen(name, "rb");
+	if (fp == NULL) {
+		fprintf(stderr, "Cannot open input file '%s'\n", name);
+		return -1;
+	}
+
+	fseek(fp, 0, SEEK_END);
+	fsize = ftell(fp);
+	fseek(fp, 0, SEEK_SET);
+
+	ptemp = (ubyte_t *) malloc(sizeof(ubyte_t) * fsize);
+
+	fread(ptemp, sizeof(ubyte_t), fsize, fp);
+	fclose(fp);
+	*buf = ptemp;
+	return fsize;
+}
+
+int writeToFile(const char *name, const ubyte_t *buf, int size)
+{
+	FILE *fp;
+
+	fp = fopen(name, "wb");
+	if (fp == NULL) {
+		fprintf(stderr, "Cannot open output file '%s'\n", name);
+		return -1;
+	}
+
+	fwrite(buf, sizeof(ubyte_t), size, fp);
+	fclose(fp);
+	return;
 }
